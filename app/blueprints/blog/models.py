38f8e1c8d0d266d9f128c25651512fab065fb8da
Apollo Johnson <amj.johnson0@gmail.com>
2021-06-24 16:22:56 -0500

    fixed createcontact issue

--- app/blueprints/blog/models.py
+++ app/blueprints/blog/models.py
@@ -9,9 +9,12 @@ class Post(db.Model):
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-    def __init__(self, title, body, user_id):
-        self.title = title
-        self.body = body
+    def __init__(self, firstname, lastname, email, address, phonenumber, user_id):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.email = email
+        self.address = address
+        self.phonenumber = phonenumber
         self.user_id = user_id
 
     def __repr__(self):
